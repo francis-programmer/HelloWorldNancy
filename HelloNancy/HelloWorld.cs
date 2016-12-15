002317e894a8a8b47de02a6b2d5101aaa9eec3ed
Francis Programmer <francis.programmer@hotmail.com>
2016-12-15 13:58:31 +0000

    Push Tag

--- HelloNancy/HelloWorld.cs
+++ HelloNancy/HelloWorld.cs
@@ -10,7 +10,7 @@ namespace HelloNancy
     {
         public HelloWorld()
         {
-            Get["/"] = _ => "Hello World, please work with tag added";
+            Get["/"] = _ => "Hello World. Tag has been pushed";
         }
     }
 }
