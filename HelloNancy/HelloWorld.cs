57fe3fe2953992a48385ec1ba5388d7dedd357bc
Francis Programmer <francis.programmer@hotmail.com>
2016-12-15 13:22:40 +0000

    Create hello world page

---
+++ HelloNancy/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace HelloNancy
+{
+    public class HelloWorld : NancyModule
+    {
+        public HelloWorld()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
